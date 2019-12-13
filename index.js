abdac5f42a9ad6405fe40f5ea153ce26c20d7b57
Sergey Dmitriev <sergey_dmitriev@epam.com>
2019-12-13 15:47:58 +0300

    Adds some functionality

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const { ApolloServer } = require('apollo-server');
 
 
-
+const photos = [];
+let _id = 0;
 
 // type description
 
+
 const typeDefs =`
+    enum PhotoCategory {
+        SELFIE
+        PORTRAIT
+        ACTION
+        LANDSCAPE
+        GRAPHIC
+    }
+
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+        category: PhotoCategory!
+    }
+
+    input PostPhotoInput {
+        name: String!
+        category: PhotoCategory=PORTRAIT
+        description: String
+    } 
+
     type Query {
-        totalPhotos: Int!
+        totalPhotos: Int!,
+        allPhotos: [Photo!]!
+    }
+
+    type Mutation {
+        postPhoto(name: String! description: String): Photo!
     }
 `;
 
@@ -15,7 +44,24 @@ const typeDefs =`
 // Resolver function returns data of denoted type
 const resolvers = {
     Query: {
-        totalPhotos: () => 42 // common name: totalPhotos
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
+    },
+    Mutation: {
+        postPhoto(parent, args) { // parent is alink to parent object (Mutation). it is always the 1st arg
+            const newPhoto = {
+                id: _id++,
+                ...args
+            }
+            
+            
+            photos.push(newPhoto);
+            
+            return newPhoto;
+        }
+    },
+    Photo: { // trivial resolver
+        url: parent => `http://yoursite.com/img/${parent.id}.jpg`
     }
 }
 
